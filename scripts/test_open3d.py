6b6275d336ef473f81bd698806c23e90d67397b0
Sarvesh Dhumal <72063635+Sarveshdhumal@users.noreply.github.com>
2026-02-04 21:41:12 +0100

    updated file

--- scripts/test_open3d.py
+++ scripts/test_open3d.py
@@ -1,7 +1,6 @@
 import open3d as o3d
 import numpy as np
 
-# Create a sample 3D point cloud
 points = np.random.rand(1000, 3)
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(points)
