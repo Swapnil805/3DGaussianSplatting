0f5c62c74b1dde1ad1274737c5c74090f220eefb
Swapnil Johri <43852317+Swapnil805@users.noreply.github.com>
2026-02-11 02:17:00 +0100

    Update visualize_colmap_sparse.py
    adding Comments

--- scripts/visualize_colmap_sparse.py
+++ scripts/visualize_colmap_sparse.py
@@ -4,11 +4,11 @@ import os
 ply_path = "../data/colmap_output/sparse/0/points3D.ply"
 
 if not os.path.exists(ply_path):
-    print(f"❌ File not found: {ply_path}")
+    print(f" File not found: {ply_path}")
 else:
-    print(f"✅ Loading point cloud from: {ply_path}")
+    print(f" Loading point cloud from: {ply_path}")
     pcd = o3d.io.read_point_cloud(ply_path)
-    print(pcd)  # prints number of points etc.
+    print(pcd)  # printing number of points
     o3d.visualization.draw_geometries(
         [pcd],
         window_name="COLMAP Sparse Reconstruction",
